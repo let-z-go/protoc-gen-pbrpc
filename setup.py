f9a63e2c19be0937ba00717f4a9877fc08082458
Roy O'Young <roy2220@outlook.com>
2019-04-04 21:52:11 +0800

    Refactor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="protoc-gen-pbrpc",
-    version="0.0.0",
+    version="0.1.0",
     description="PBRPC stub generator",
     scripts=["bin/protoc-gen-pbrpc"],
     install_requires=["protobuf"],
